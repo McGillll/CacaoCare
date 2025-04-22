a0a55de3d36ae817765a041e7b5ca3e1d4f46f02
McGillll <mcgilibag123@gmail.com>
2025-04-22 16:04:35 +0800

    getAllUser function added

--- composables/api/sevices/UserService.ts
+++ composables/api/sevices/UserService.ts
@@ -2,7 +2,9 @@ import BaseAPIService from "../BaseApiService";
 import { authService } from "./AuthService";
 
 class UserService extends BaseAPIService {
-    
+    async getAllUser(): Promise<any> {
+        return await this.request('/user', 'GET')
+    }
 }
 
 export const userService = new UserService();
