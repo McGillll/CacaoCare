f3d9f14850a76f35086b7b0c3c2c0f34eb84df92
McGillll <mcgilibag123@gmail.com>
2025-05-05 17:17:17 +0800

    props use

--- components/featuresection/items.vue
+++ components/featuresection/items.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="p-8 flex flex-col items-center justify-center border rounded-lg shadow hover:shadow-lg transition-all duration-300">
         <slot name="icon"/>
-        <h3 class="text-xl font-bold my-2">AI-Powered Detection</h3>
-        <p class="text-gray-600">Quickly identify cacao diseases with state-of-the-art convolutional neural networks.</p>
+        <h3 class="text-xl font-bold my-2">{{ props.feature }}</h3>
+        <p class="text-gray-600">{{ props.description }}</p>
     </div>
 </template>
 
