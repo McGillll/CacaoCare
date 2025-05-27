905aa4ce8275f9ee8d69d74b881629749c61c35b
Makhilll <makhillibag@gmail.com>
2025-05-27 22:44:28 +0800

    check animation modify

--- components/notification/successfull.vue
+++ components/notification/successfull.vue
@@ -6,24 +6,24 @@
       <transition enter-active-class="transform ease-out duration-300 transition" enter-from-class="translate-y-2 opacity-0 sm:translate-y-0 sm:translate-x-2" enter-to-class="translate-y-0 opacity-100 sm:translate-x-0" leave-active-class="transition ease-in duration-100" leave-from-class="opacity-100" leave-to-class="opacity-0">
         <div v-if="props.show" class="pointer-events-auto w-full max-w-sm overflow-hidden rounded-lg bg-white shadow-lg ring-1 ring-black/5">
           <div class="p-4">
-            <div class="flex items-start">
-              <div class="shrink-0">
-                <CheckCircleIcon class="size-6 text-green-400" aria-hidden="true" />
+            <div class="flex flex-col">
+              <div class="flex w-full gap-2">
+                <div class="shrink-0">
+                  <svg class="checkmark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52"><circle class="checkmark__circle" cx="26" cy="26" r="25" fill="none" /><path class="checkmark__check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8" /></svg>
+                </div>
+                <p class="text-sm mr-auto font-medium text-gray-900">Successfully saved!</p>
+                <div class="ml-4 flex shrink-0">
+                  <button 
+                    type="button" 
+                    @click="emit('close')" 
+                    class="inline-flex rounded-md bg-white text-gray-400 hover:text-gray-500 focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:outline-hidden"
+                  >
+                    <span class="sr-only">Close</span>
+                    <XMarkIcon class="size-5" aria-hidden="true" />
+                  </button>
+                </div>
               </div>
-              <div class="ml-3 w-0 flex-1 pt-0.5">
-                <p class="text-sm font-medium text-gray-900">Successfully saved!</p>
-                <p @click="reload()" class="cursor-pointer underline mt-1 text-sm text-gray-500">Click to reload page.</p>
-              </div>
-              <div class="ml-4 flex shrink-0">
-                <button 
-                  type="button" 
-                  @click="emit('close')" 
-                  class="inline-flex rounded-md bg-white text-gray-400 hover:text-gray-500 focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:outline-hidden"
-                >
-                  <span class="sr-only">Close</span>
-                  <XMarkIcon class="size-5" aria-hidden="true" />
-                </button>
-              </div>
+              <p @click="reload()" class="cursor-pointer underline mt-1 ml-7 text-sm text-gray-500">Click to reload page.</p>
             </div>
           </div>
         </div>
@@ -33,7 +33,6 @@
 </template>
 
 <script setup>
-import { CheckCircleIcon } from '@heroicons/vue/24/outline'
 import { XMarkIcon } from '@heroicons/vue/20/solid'
 import { defineEmits } from 'vue';
 
@@ -46,5 +45,58 @@ const emit = defineEmits(['close']);
 const props = defineProps({
     show:Boolean
 })
-
 </script>
+
+<style lang="css">
+
+.checkmark {
+    width: 20px;
+    height: 20px;
+    border-radius: 50%;
+    display: block;
+    stroke-width: 2;
+    stroke: #4bb71b;
+    stroke-miterlimit: 10;
+    box-shadow: inset 0px 0px 0px #4bb71b;
+    animation: fill .4s ease-in-out .4s forwards, scale .3s ease-in-out .9s both;
+}
+.checkmark__circle {
+    stroke-dasharray: 166;
+    stroke-dashoffset: 166;
+    stroke-width: 2;
+    stroke-miterlimit: 10;
+    stroke: #4bb71b;
+    fill: #fff;
+    animation: stroke 0.6s cubic-bezier(0.65, 0, 0.45, 1) forwards;
+ 
+}
+
+.checkmark__check {
+    transform-origin: 50% 50%;
+    stroke-dasharray: 48;
+    stroke-dashoffset: 48;
+    animation: stroke 0.3s cubic-bezier(0.65, 0, 0.45, 1) 0.8s forwards;
+}
+
+@keyframes stroke {
+    100% {
+        stroke-dashoffset: 0;
+    }
+}
+
+@keyframes scale {
+    0%, 100% {
+        transform: none;
+    }
+
+    50% {
+        transform: scale3d(1.1, 1.1, 1);
+    }
+}
+
+@keyframes fill {
+    100% {
+        box-shadow: inset 0px 0px 0px 30px #4bb71b;
+    }
+}
+</style>
